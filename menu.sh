f9c9c6b6bba49fbbec13bafb7aca50c4f5ee9dfb
Justin Paul <justin@jpaul.me>
2018-06-22 17:43:53 -0400

    menu updates for git refresh

--- menu.sh
+++ menu.sh
@@ -34,7 +34,7 @@ do
           1) # Update zPlanner Scripts from Github
               clear
 	      echo "Updating zPlanner from github"
-	      (cd /home/zerto/zplanner/ && git reset --hard HEAD && git pull http://www.github.com/recklessop/zplanner/)
+	      (cd /home/zerto/zplanner/ && git reset --hard HEAD && git pull http://www.github.com/zerto-ta-public/zplanner/)
               ;;
           2) # Config Network Settings
 	      clear
