d9a847182d9194d34a6612c2fbfd7e5992fd6ec6
Justin Paul <justin@jpaul.me>
2018-06-22 17:32:59 -0400

    scale out work

--- loaders/tocsv.php
+++ loaders/tocsv.php
@@ -4,9 +4,9 @@ $csvpath = '/home/zerto/data/';
 
 //delete old CSV files
 for ($i = 0; $i < 10; $i++) {
-	$file = $csvpath . "vmlist" . $i . ".csv";
-	unlink($file);
-	echo "Deleted " . $file . PHP_EOL;
+        $file = $csvpath . "vmlist" . $i . ".csv";
+        unlink($file);
+        echo "Deleted " . $file . PHP_EOL;
 }
 
 //connect to the database
@@ -14,36 +14,49 @@ for ($i = 0; $i < 10; $i++) {
 $connect = mysqli_connect("localhost","root","Zertodata1!");
 mysqli_select_db($connect,"zerto"); //select the table
 
+// get the total number of VMs to be monitored and store in count
 $result = mysqli_query($connect, "SELECT * FROM `vms` WHERE `monitor` = 'Y'") or die(mysqli_error($connect));
 $count = mysqli_num_rows($result) or die(mysqli_error($connect));
 echo $count . PHP_EOL;
+// if monitoring 25 vms or less only use 1 worker
 if ($count < 26) {
-	$csvfile = $csvpath . "vmlist0.csv";
-	echo $csvfile . PHP_EOL;
-	$handle = fopen($csvfile, 'w') or die('Cannot open file:  '.$csvfile); //implicitly creates file
-	$line1 = '"Name"' . PHP_EOL;
-	fwrite($handle, $line1);
+        // store all vms in vmlist0
+        $csvfile = $csvpath . "vmlist0.csv";
+        echo $csvfile . PHP_EOL;
+        $handle = fopen($csvfile, 'w') or die('Cannot open file:  '.$csvfile); //implicitly creates file
+        // write header to csv file
+        $line1 = '"Name"' . PHP_EOL;
+        fwrite($handle, $line1);
 
-	$vms = mysqli_query($connect, "SELECT `name` FROM `vms` WHERE `monitor` = 'Y' ORDER BY `name`") or die(mysqli_error($connect));
-	while($row = mysqli_fetch_assoc($vms)) {
-		$data = '"' .  $row["name"] . '"' . PHP_EOL;
-		fwrite($handle, $data);
-	}
+        //go get the VM names from mysql and put them in Csv
+        $vms = mysqli_query($connect, "SELECT `name` FROM `vms` WHERE `monitor` = 'Y' ORDER BY `name`") or die(mysqli_error($connect));
+        while($row = mysqli_fetch_assoc($vms)) {
+                $data = '"' .  $row["name"] . '"' . PHP_EOL;
+                fwrite($handle, $data);
+        }
 } else {
-	$perworker = ceil($count / 10);
-	for ($i = 0; $i < 10; $i++) {
-		$csvfile = "vmlist" . $i . ".csv";
-		$csvfile = $csvpath . $csvfile;
-		echo $csvfile . PHP_EOL;
-		$handle = fopen($csvfile, 'w') or die('Cannot open file:  '.$csvfile); //implicitly creates file
-		$line1 = '"Name"' . PHP_EOL;
-		fwrite($handle, $line1);
-		$x = $i * $perworker;
-		$vms = mysqli_query($connect, "SELECT `name` FROM `vms` WHERE `monitor` = 'Y' ORDER BY `name` LIMIT $x, $perworker") or die(mysqli_error($connect));
-        	while($row = mysqli_fetch_assoc($vms)) {
-	               	$data = '"' .  $row["name"] . '"' . PHP_EOL;
-                	fwrite($handle, $data);
-		}
+
+        // if we have more than 25 vms then we need to figure out how many VMs each worker will have to do. SO total vm count / 10 = number per worker
+        $perworker = ceil($count / 10);
+        for ($i = 0; $i < 10; $i++) {
+                // create a csv file with a 1-10 number before the extension
+                $csvfile = "vmlist" . $i . ".csv";
+                $csvfile = $csvpath . $csvfile;
+                echo $csvfile . PHP_EOL;
+                $handle = fopen($csvfile, 'w') or die('Cannot open file:  '.$csvfile); //implicitly creates file
+                // write header to csv file
+                $line1 = '"Name"' . PHP_EOL;
+                fwrite($handle, $line1);
+                // figure out where mysql should start looking for VMs
+                $x = $i * $perworker;
+                // get the right vms to put in this csv file and write them to the csv
+                $vms = mysqli_query($connect, "SELECT `name` FROM `vms` WHERE `monitor` = 'Y' ORDER BY `name` LIMIT $x, $perworker") or die(mysqli_error($connect));
+                while($row = mysqli_fetch_assoc($vms)) {
+                        $data = '"' .  $row["name"] . '"' . PHP_EOL;
+                        fwrite($handle, $data);
+                }
         }
 }
+
+
 
--- workers/vm-getio.ps1
+++ workers/vm-getio.ps1
@@ -4,13 +4,17 @@
 /usr/bin/php /home/zerto/zplanner/loaders/tocsv.php
 
 #start getio workers
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio0.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio1.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio2.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio3.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio4.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio5.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio6.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio7.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio8.ps1"
-start-job -FilePath "/home/zerto/zplanner/workers/getio/vm-getio9.ps1"
+start-job -Name GetIO0 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio0.ps1}
+start-job -Name GetIO1 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio1.ps1}
+start-job -Name GetIO2 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio2.ps1}
+start-job -Name GetIO3 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio3.ps1}
+start-job -Name GetIO4 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio4.ps1}
+start-job -Name GetIO5 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio5.ps1}
+start-job -Name GetIO6 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio6.ps1}
+start-job -Name GetIO7 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio7.ps1}
+start-job -Name GetIO8 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio8.ps1}
+start-job -Name GetIO9 -ScriptBlock {pwsh /home/zerto/zplanner/workers/getio/vm-getio9.ps1}
+
+
+# wait for all jobs to complete before exiting session
+Get-Job | Wait-Job
