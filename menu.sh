510bf2b2e692948ed440e507677bb41b71c676d3
Justin Paul <justin@jpaul.me>
2018-06-21 21:59:29 -0400

    worked on menu.sh

--- menu.sh
+++ menu.sh
@@ -33,7 +33,7 @@ do
           1) # Update zPlanner Scripts from Github
               clear
 	      echo "Updating zPlanner from github"
-	      git pull https://github.com/zerto-ta-public/zplanner/ /home/zerto/zplanner
+	      git-dir=/home/zerto/zplanner/.git pull
               ;;
           2) # Config Network Settings
               echo "Network config stuff here... later on."
