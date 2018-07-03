b8d3ee28ebbb44cb0982ac6692f4e30fc62e20a6
Justin Paul <justin@jpaul.me>
2018-07-02 22:52:08 -0400

    fixed error when inserting duplicate vm to inventory

--- loaders/loadvms.php
+++ loaders/loadvms.php
@@ -18,7 +18,7 @@ echo "Row Data \n";
 while ($data = fgetcsv($handle,1000, ",", '"')) {
 	echo $data[0] . "\n";
         if ( $data[0] ) {
-            mysqli_query($connect, "INSERT INTO vms (name, monitor) VALUES
+            mysqli_query($connect, "INSERT IGNORE INTO vms (name, monitor) VALUES
                 (
                     '".addslashes($data[0])."',
                     'N'
