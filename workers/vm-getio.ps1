99f96de3558d16f8466a780c147a3be524ea2105
Justin Paul <justin@jpaul.me>
2018-10-24 16:37:30 -0400

    Update vm-getio.ps1
    corrected day time for 24 hour format

--- workers/vm-getio.ps1
+++ workers/vm-getio.ps1
@@ -47,7 +47,7 @@ While ($lockstatus -ne 1)
 /usr/bin/php /home/zerto/zplanner/loaders/tocsv.php
 
 #generate datetime for this run
-$datetime = get-date -Format "yyyy-MM-dd hh:mm:ss"
+$datetime = get-date -Format "yyyy-MM-dd HH:mm:ss"
 $datetime | Out-File $datetimefile
 
 #start getio workers
