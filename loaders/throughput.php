19197b9fffb8f9a6a415689d11da8ca161e62e05
Justin Paul <justin@jpaul.me>
2018-10-23 20:03:25 -0400

    Update throughput.php

--- loaders/throughput.php
+++ loaders/throughput.php
@@ -4,11 +4,11 @@ mysqli_select_db($connect,"zerto"); //select the table
 
 	$getdate = "SELECT datestamp FROM `stats` GROUP BY datestamp DESC LIMIT 1";
 	$date = mysqli_query($connect, $getdate) or die (mysqli_error($connect));
-	$date = $date->fetch_object()
+	$date = $date->fetch_object();
 	echo $date;
 	$getmbps = "SELECT (SUM(`KBWriteAvg`) * 8 / 1024) FROM `stats` WHERE datestamp = '$date'";
 	$mbps = mysqli_query($connect, $mbps) or die (mysqli_error($connect));
-	$mbps = $mbps->fetch_object()
+	$mbps = $mbps->fetch_object();
 	echo $mbps;
 	mysqli_query($connect,
 		"INSERT INTO `throughput` (time, WriteMbps) VALUES ( '$date', '$mbps')"
