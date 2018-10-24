787bdb99253ae61dc5024bbbdf1f51e3c2cf664c
Justin Paul <justin@jpaul.me>
2018-10-23 20:27:13 -0400

    Update throughput.php

--- loaders/throughput.php
+++ loaders/throughput.php
@@ -2,15 +2,17 @@
 $connect = mysqli_connect("localhost","root","Zertodata1!");
 mysqli_select_db($connect,"zerto"); //select the table
 
-	$getdate = "SELECT datestamp FROM `stats` GROUP BY datestamp DESC LIMIT 1";
-	$date = mysqli_query($connect, $getdate) or die (mysqli_error($connect));
-	$date = $date->fetch_object();
-	echo $date;
-	$getmbps = "SELECT (SUM(`KBWriteAvg`) * 8 / 1024) FROM `stats` WHERE datestamp = '$date'";
-	$mbps = mysqli_query($connect, $mbps) or die (mysqli_error($connect));
-	$mbps = $mbps->fetch_object();
-	echo $mbps;
-	mysqli_query($connect,
-		"INSERT INTO `throughput` (time, WriteMbps) VALUES ( '$date', '$mbps')"
-		) or die (mysqli_error($connect));
+        $getdate = "SELECT datestamp FROM `stats` GROUP BY datestamp DESC LIMIT 1";
+        $date = mysqli_query($connect, $getdate) or die (mysqli_error($connect));
+        $date = $date->fetch_array();
+        $date = $date['datestamp'];
+        echo $date;
+        $getmbps = "SELECT (SUM(`KBWriteAvg`) * 8 / 1024) as mbps FROM `stats` WHERE datestamp = '$date'";
+        $mbps = mysqli_query($connect, $getmbps) or die (mysqli_error($connect));
+        $mbps = $mbps->fetch_array();
+        $mbps = $mbps['mbps'];
+        echo $mbps;
+        mysqli_query($connect,
+                "INSERT INTO `throughput` (time, WriteMbps) VALUES ( '$date', '$mbps')"
+                ) or die (mysqli_error($connect));
 ?>
