cf633fdace5c87a0bbf32a05d28655064179e7f9
Justin Paul <justin@jpaul.me>
2018-11-20 15:58:18 -0500

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -6,7 +6,7 @@ mkdir -p /home/zerto/logs
 # Rev 1.1 Create log cleanup job and run the throughput table script retro actively
 logs=$(crontab -l | grep -ic logs)
 if [ $logs = 1 ] then
-        echo Log cleanup already installed
+        echo "Log cleanup already installed"
 else
         echo Installing log cleanup job
         line="@daily /usr/bin/find /home/zerto/logs -mtime +7 -type f -delete"
@@ -19,7 +19,7 @@ fi
 # Rev 1.2 Add nightly update scripts
 nightly=$(crontab -l | grep -ic nightly)
 if [ $nightly = 1 ] then
-        echo Nightly Update job already installed
+        echo "Nightly Update job already installed"
 else
         echo Installing nightly update  job
         line="@daily /bin/bash /home/zerto/modules/nightlyupdate.sh"
