5e229480683ca51fc072a3da5dd9b6f3717c026d
Justin Paul <justin@jpaul.me>
2019-01-10 10:43:13 -0400

    Delete backup-updater.sh
    Remove file that was never implemented

--- workers/backup-updater.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-########################################################
-# This Script is for doing backup Updates to zPlanner 
-########################################################
-
-# version 3.0.7 updates
-# update MySQL Scheme to Support VPG group information
-
